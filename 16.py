83b0584bfba025c2ac7ba6e3521ddd3917153f40
vadranamakhila <42572588+vadranamakhila@users.noreply.github.com>
2018-12-13 10:53:43 +0530

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,8 @@
+lower,upper=map(int,raw_input().split())
+for num in range(lower,upper+1):
+  if num>1:
+      for i in range(2,num):
+          if (num%i==0):
+                 break
+          else:
+                  print num ,
